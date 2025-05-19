ea9f4115d5a53e7f354e0b5a604ef0db91941fcc
Fantastix80 <jean.vanwittenberge@gmail.com>
2025-05-19 15:12:32 +0200

    Initialisation du projet

---
+++ src/tp1/utils/lib.py
@@ -0,0 +1,13 @@
+def hello_world() -> str:
+    """
+    Hello world function
+    """
+    return "hello world"
+
+
+def choose_interface() -> str:
+    """
+    Return network interface and input user choice
+    """
+    interface = ""
+    return interface
